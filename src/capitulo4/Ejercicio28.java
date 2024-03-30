c5cb538e8674b7e7cff65908d0bcb4395c666c39
Derwin Arenas <dearenas@bancolombia.com.co>
2024-03-30 16:26:07 -0500

    Ejercicios 28

--- src/capitulo4/Ejercicio28.java
+++ src/capitulo4/Ejercicio28.java
@@ -4,12 +4,69 @@ public class Ejercicio28 {
 
     public static void main(String[] args) {
 
+        int x = 5;
+        int y = 7;
+
+        /**
+         * Literal a
+         * Salida: @@@@@ $$$$$ &&&&&
+         */
+        /*if (y == 8)
+            if (x == 5)
+                System.out.println("@@@@@");
+            else
+                System.out.println("#####");
+        System.out.println("$$$$$");
+        System.out.println("&&&&&");*/
+
+        /**
+         * Literal b.
+         * Salida: @@@@@ $$$$$
+         */
+        /*if (y == 8) {
+
+            if (x == 5) {
+
+                System.out.println("@@@@@");
+            } else {
+                System.out.println("#####");
+            }
+            System.out.println("$$$$$");
+        }else{
+            System.out.println("&&&&&");
+        }*/
+
+        /**
+         * Literal c
+         * Salida: @@@@@
+         */
+        /*if (y == 8) {
+
+            if (x == 5)
+                System.out.println("@@@@@");
+            else {
+                System.out.println("#####");
+                System.out.println("$$$$$");
+                System.out.println("&&&&&");
+            }
+        }*/
+
+        /**
+         * Literal D.
+         * Salida: ##### $$$$$ &&&&&
+         */
+        if (y == 8) {
+            if (x == 5) {
+                System.out.println("@@@@@");
+            } else {
+
+            }
+        }
+        System.out.println("#####");
+        System.out.println("$$$$$");
+        System.out.println("&&&&&");
 
 
 
-
-    }
-
-
-
+    }// main
 }
