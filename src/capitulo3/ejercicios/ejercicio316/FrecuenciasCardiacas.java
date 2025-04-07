f1ab6b5ca19b1c6a33b70a23f01174adc9e1e7f3
derwinarenas7 <derwinarenas7@gmail.com>
2025-04-07 16:55:39 -0500

    ejericicios cap 3

--- src/capitulo3/ejercicios/ejercicio316/FrecuenciasCardiacas.java
+++ src/capitulo3/ejercicios/ejercicio316/FrecuenciasCardiacas.java
@@ -2,6 +2,7 @@ package capitulo3.ejercicios.ejercicio316;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Arrays;
 
 public class FrecuenciasCardiacas {
 
@@ -47,6 +48,48 @@ public class FrecuenciasCardiacas {
     public int calculateAge(LocalDate actual) {
         return Period.between(dateOfBirth, actual).getYears();
 
+    }
+
+    public int calculateMaximunHeartRate() {
+
+        int CONSTANTE = 220;
+        int maximunHearRate = CONSTANTE - calculateAge(LocalDate.now());
+
+        return maximunHearRate;
+
+    }
+
+    public String[] calculateExpectedHeartRate() {
+
+        double minValue = 0.5;
+        double value1 = 0.55;
+        double value2 = 0.60;
+        double value3 = 0.65;
+        double value4 = 0.70;
+        double value5 = 0.75;
+        double value6 = 0.80;
+        double maxValue = 0.85;
+
+        double expectedHeartRate1 = calculateMaximunHeartRate() * minValue;
+        double exp1 = calculateMaximunHeartRate() * value1;
+        double exp2 = calculateMaximunHeartRate() * value2;
+        double exp3 = calculateMaximunHeartRate() * value3;
+        double exp4 = calculateMaximunHeartRate() * value4;
+        double exp5 = calculateMaximunHeartRate() * value5;
+        double exp6 = calculateMaximunHeartRate() * value6;
+        double expectedHeartRate2 = calculateMaximunHeartRate() * maxValue;
+
+        double[] arreglo = new double[8];
+        arreglo[0] = expectedHeartRate1;
+        arreglo[1] = exp1;
+        arreglo[2] = exp2;
+        arreglo[3] = exp3;
+        arreglo[4] = exp4;
+        arreglo[5] = exp5;
+        arreglo[6] = exp6;
+        arreglo[7] = expectedHeartRate2;
+
+        return new String[]{Arrays.toString(arreglo)};
     }
 
 
--- src/capitulo3/ejercicios/ejercicio316/TestFrecuenciasCardiacas.java
+++ src/capitulo3/ejercicios/ejercicio316/TestFrecuenciasCardiacas.java
@@ -3,19 +3,38 @@ package capitulo3.ejercicios.ejercicio316;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class TestFrecuenciasCardiacas {
 
     public static void main(String[] args) {
 
-        FrecuenciasCardiacas frecuenciasCardiacas = new FrecuenciasCardiacas(
-                "Derwin",
-                "Arenas",
-                LocalDate.of(1995, 1, 9)
-        );
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("La edad es: " + frecuenciasCardiacas.calculateAge(LocalDate.now()));
+        //Solicitud de la información
+        System.out.println("Ingrese el nombre del paciente: ");
+        String nombre = sc.nextLine();
 
+        System.out.println("Ingrese el apellido del paciente: ");
+        String apellido = sc.nextLine();
+
+        System.out.println("Ingrese el año de nacimiento del paciente: ");
+        int anioNacimiento = sc.nextInt();
+        System.out.println("Ingrese el mes de nacimiento del paciente: ");
+        int mesNacimiento = sc.nextInt();
+        System.out.println("Ingrese el dia de nacimiento del paciente: ");
+        int diaNacimiento = sc.nextInt();
+
+        FrecuenciasCardiacas frecuenciasCardiacas = new FrecuenciasCardiacas(nombre, apellido, LocalDate.of(
+                anioNacimiento, mesNacimiento, diaNacimiento
+        ));
+
+
+        Logger logger = Logger.getLogger(String.valueOf(TestFrecuenciasCardiacas.class));
+        logger.info("La edad es: %s años".formatted(frecuenciasCardiacas.calculateAge(LocalDate.now())));
+        logger.info("Frecuencia cardiaca máxima: %d".formatted(frecuenciasCardiacas.calculateMaximunHeartRate()));
+        logger.info("rango de frecuencias cardiacas esperadas: %s".formatted(frecuenciasCardiacas.calculateExpectedHeartRate()));
 
 
 
